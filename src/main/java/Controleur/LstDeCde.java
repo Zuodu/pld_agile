63572b99962e68af23934ca21ee29c35cbf9be4b
Manuel Amouroux <manuel.amouroux@insa-lyon.fr>
2017-12-04 10:29:29 +0100

    Constructeurs de copie

--- src/main/java/Controleur/LstDeCde.java
+++ src/main/java/Controleur/LstDeCde.java
@@ -5,6 +5,11 @@ import java.util.LinkedList;
 public class LstDeCde {
 	private LinkedList<Commande> liste;
 	private int i;
+	public LstDeCde()
+	{
+		i = 0;
+		liste = new LinkedList<Commande>();
+	}
 	public void undo() {
 		if(i>=0)
 			liste.get(i--).undoCde();
