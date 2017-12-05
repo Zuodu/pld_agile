3665a497f724fbe97c0c2baeeb6a3ba9f9431a77
zifanYao <zuoduzuodu24@gmail.com>
2017-12-05 23:02:52 +0100

    update IHM design & color, ajout des images, update visuel feuille de route

--- src/main/java/FeuilleDeRoute/Indication.java
+++ src/main/java/FeuilleDeRoute/Indication.java
@@ -5,8 +5,8 @@ public enum Indication {
     ADroite("Tournez à droite pour aller sur"),
     AGauche("Tournez à gauche pour aller sur"),
     Demarrage("Démarrez et allez sur"),
-    FinItineraire("Stoppez et terminez l'itinéraire numéro"),
-    DebutItineraire("Début de l'itinéraire numéro"),
+    FinItineraire("Stoppez et terminez la livraison numéro"),
+    DebutItineraire("Début de la livraison numéro"),
     FinTournee("Stoppez à l'entrepôt et terminez la tournée");
 
     private String texte;
